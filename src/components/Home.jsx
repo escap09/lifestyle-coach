b647832a9e740062d0c9abb1ea68956486b99020
Sumit Kumar <sumitkumar@Sumits-MBP.attlocal.net>
2018-12-29 21:05:34 -0600

    updated images/layout/add content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,33 +5,39 @@ export default class Home extends Component {
     render() {
         return (
             <div className='container'>
-                <div className='jumbotron shadow'>
+                <div className='jumbotron shadow jumbotron-home'>
                     <h2>Get fit - physically, mentally & emotionally</h2>
-                    <p>All you need is the right diet, some exercise and a lot of motivation.</p>
+                    <p className='text-dark'>All you need is the right diet, some exercise and a lot of motivation.</p>
+                    <br />
                     <a className="btn btn-info shadow" href="/about" role="button">About Me</a>
                 </div>
                 <div className='show-grid row'>
                     <div className='col-md-4'>
-                        <img src='/assets/IMG20171221083154.jpg' className='profile-pic' alt=''/>
+                        <img src='/assets/home-1.jpg' className='profile-pic' alt='' />
                     </div>
                     <div className='col-md-7'>
-                        <h4>My definition of fitness</h4>
+                        <h5>My Fitness Mantra</h5>
                         <p>
-                        Fitness is not about losing weight, doing lot of exercise or starving.
-                        My definition of fitness is, you can eat what you like, drink what you like, be
-                        active the way you want, wear the dress of your choice and start loving yourself.
-                        So basically, your mind and body should give you the freedom of doing what you love.
-                        Fitness is for one and all - be it housewife, working professional, senior citizen,
-                        midlife people, kids & youngsters, fashion & modeling aspirants, sports person,
-                        entrepreneurs or senior executives. In a broader sense, we should treat fitness like our
-                        fundamental right. We have right to education, equality, speech etc then why not - Right to fitness!
+                            Fitness is not about losing weight, doing lot of exercise or starving.
                         </p>
                         <p>
-                            But this right shouldn't come from society, it should come from within.
+                            My definition of fitness is, you can eat what you like, drink what you like <span className='font-italic'>(with some tricks)</span>, be
+                        active the way you want, wear the dress of your choice and start loving yourself.
+                        So basically, your mind and body should give you the freedom of doing what you love.
+                        </p>
+                        <p>
+                            Fitness is for one and all - be it housewife, working professional, senior citizen,
+                        midlife people, kids & youngsters, fashion & modeling aspirants, sports person,
+                        entrepreneurs or senior executives. In a broader sense, we should treat fitness like our
+                        fundamental right. We have right to education, equality, speech etc then why not <strong className='text-dark'>- Right to Fitness!</strong>
+                        </p>
+
+                        <p>
+                            <strong className='text-dark'> But this right shouldn't come from society, it should come from within.</strong>
                         </p>
                         <br />
                         <p>
-                            So, realize your Right to Fitness and get going!!
+                            <strong className='text-dark font-weight-bold'>So, realize your Right to Fitness and get going!!</strong>
                         </p>
                         <hr />
                         <a className="btn btn-warning shadow" href="/contact" role="button">Get In Touch</a>
