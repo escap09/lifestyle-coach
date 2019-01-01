50c4c0293ab4eeb14bf0f02d99aae0fa2429bc2f
Sumit Kumar <sumitkumar@Sumits-MBP.attlocal.net>
2019-01-01 16:26:54 -0600

    fixed testimonials

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -55,36 +55,48 @@ export default class Home extends Component {
                     <h4 align='center'>Testimonials</h4>
                     <br />
 
-                    <div id='carouselExampleControls' class="carousel slide jumbotron" data-ride="carousel">
+                    <div id='carouselExampleControls' class="carousel slide" data-ride="carousel">
                         <div class="carousel-inner" align='center'>
                             <div class="carousel-item active">
                                 <div class="card text-white mb-3 shadow">
                                     <div class="card-header text-dark">Shahid Shaikh</div>
                                     <div class="card-body">
+                                        <div className='col-md-4'>
+                                            <img src='/assets/test-1.jpg' className='w-100 carousel-img' alt='testimonial1' />
+                                        </div>
+                                        <br />
                                         <h5 class="card-title">Weight Gain Program</h5>
-                                        <br/>
+                                        <br />
                                         <p class="card-text">
-                                        Chandni is knowledgeable about diet and workout at the same time. She does it with the passion so results are always best.
+                                            Chandni is knowledgeable about diet and workout at the same time. She does it with the passion so results are always best.
                                         </p>
                                     </div>
                                 </div>
                             </div>
                             <div class="carousel-item">
-                            <div class="card text-white mb-3 shadow">
+                                <div class="card text-white mb-3 shadow">
                                     <div class="card-header text-dark">Khushboo Arora</div>
                                     <div class="card-body">
+                                        <div className='col-md-4'>
+                                            <img src='/assets/test-2.jpg' className='w-100 carousel-img' alt='testimonial2' />
+                                        </div>
+                                        <br />
                                         <h5 class="card-title">Weight Loss Program</h5>
-                                        <br/>
+                                        <br />
                                         <p class="card-text">Chandni is an awesome motivator. As I am busy professional its difficult for me to go for a strict diet but she knows the trick.</p>
                                     </div>
                                 </div>
                             </div>
                             <div class="carousel-item">
-                            <div class="card text-white mb-3 shadow">
+                                <div class="card text-white mb-3 shadow">
                                     <div class="card-header text-dark">Sumit Kumar</div>
                                     <div class="card-body">
+                                        <div className='col-md-4'>
+                                            <img src='/assets/test-3.jpg' className='w-100 carousel-img' alt='testimonial3' />
+                                        </div>
+                                        <br />
                                         <h5 class="card-title">Keto Diet Program</h5>
-                                        <br/>
+                                        <br />
                                         <p class="card-text">The best part of going through this program is that I never felt starving and still losing inches.</p>
                                     </div>
                                 </div>
