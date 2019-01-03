4fb8b23ebb9b879b7d7553b581d745726ac56916
Sumit Kumar <sumitkumar@Sumits-MBP.attlocal.net>
2019-01-02 21:11:45 -0600

    bug fixes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,10 +17,10 @@ export default class Home extends Component {
                 </div>
                 <div className='container'>
                     <div className='show-grid row'>
-                        <div className='col-md-4'>
-                            <img src='/assets/home-1.jpg' className='profile-pic img-custom-shadow' alt='home profile' />
+                        <div className='col-md-3'>
+                            <img src='/assets/home.jpg' className='profile-pic img-custom-shadow' alt='home profile' />
                         </div>
-                        <div className='col-md-7'>
+                        <div className='col-md-8'>
                             <h5>My Fitness Mantra</h5>
                             <p>
                                 Fitness is not about losing weight, doing lot of exercise or starving.
@@ -36,7 +36,6 @@ export default class Home extends Component {
                         entrepreneurs or senior executives. In a broader sense, we should treat fitness like our
                         fundamental right. We have right to education, equality, speech etc then why not <strong className='text-dark'>- Right to Fitness!</strong>
                             </p>
-
                             <p>
                                 <strong className='text-dark'> But this right shouldn't come from society, it should come from within.</strong>
                             </p>
@@ -44,7 +43,7 @@ export default class Home extends Component {
                             <p>
                                 <strong className='text-dark font-weight-bold'>So, realize your Right to Fitness and get going!!</strong>
                             </p>
-                            <hr />
+                            {/* <hr /> */}
                             <a className="btn btn-info shadow" href="/contact" role="button">Get In Touch</a>
                         </div>
                     </div>
